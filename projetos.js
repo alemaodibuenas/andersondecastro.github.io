e205aa0932e48360d4213fefa39c3b85c3ef39a9
andersondecastro <anderson.in.net@gmail.com>
2013-05-14 13:37:29 -0400

    envio de alguns projetos

---
+++ projetos.js
@@ -0,0 +1,30 @@
+var projetos = {
+	{
+		nome: "Quanto Tempo Leva",
+		tipo: "Aplicativo (Facebook)",
+		finalidade: "Estudo e teste",
+		fase: "beta"
+		hospedado: true
+	},
+	{
+		nome: "JxRotate",
+		tipo: "Plugin JS",
+		finalidade: "Estudo e uso pessoal",
+		fase: "beta",
+		hospedado: true
+	},
+	{
+		nome: "JxSlide",
+		tipo: "Plugin JS",
+		finalidade: "Estudos apenas",
+		fase: "beta",
+		hospedado: true
+	},
+	{
+		nome: "PloomaLabs",
+		tipo: "Projeto de Experimentos e Aplicações",
+		finalidade: "Ensino",
+		fase: "em uso",
+		hospedado: true
+	}
+}
